523d13666ce908ab1ab37645a3a0d8d26f738898
Abhinav Garg <abhinav.garg@clever.com>
2015-02-04 14:13:12 -0800

    Add a script to run the script with env vars.

---
+++ run_main.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+/usr/local/bin/postgres-to-redshift \
+-redshifthost=$REDSHIFT_HOST \
+-redshiftport=$REDSHIFT_PORT \
+-redshiftuser=$REDSHIFT_USER \
+-redshiftpassword=$REDSHIFT_PASSWORD \
+-redshiftdatabase=$REDSHIFT_DATABASE \
+-postgreshost=$POSTGRES_HOST \
+-postgresdatabase=$POSTGRES_DATABASE \
+-postgresuser=$POSTGRES_USER \
+-postgresport=$POSTGRES_PORT \
+-postgrespassword=$POSTGRES_PASSWORD \
+-s3prefix=$S3PREFIX \
+-tables=$TABLES_TO_COPY
